a0d26783489fc5b453730ed29069c5943037cbad
Tomas Fernandez Lobbe <tflobbe@gmail.com>
2011-07-22 19:16:55 +0000

    Resolved issue #78. Now External query executor works with many filter queries.

--- sources/solrmeter/src/main/java/com/plugtree/solrmeter/model/statistic/QueryLogStatistic.java
+++ sources/solrmeter/src/main/java/com/plugtree/solrmeter/model/statistic/QueryLogStatistic.java
@@ -35,143 +35,150 @@ import com.plugtree.solrmeter.model.exception.QueryException;
  */
 @StressTestScope
 public class QueryLogStatistic implements QueryStatistic {
-	
-	private static int maxStored;
-	
-	private List<QueryLogValue> queries;
-	
-	@Inject
-	public QueryLogStatistic() {
-		super();
-		maxStored = Integer.parseInt(SolrMeterConfiguration.getProperty("solr.queryLogStatistic.maxStored", "400"));
-		queries = Collections.synchronizedList(new LinkedList<QueryLogValue>());
-	}
-
-	@Override
-	public void onExecutedQuery(QueryResponse response, long clientTime) {
-		addToList(new QueryLogValue(response));
-	}
-
-	@Override
-	public void onFinishedTest() {}
-
-	@Override
-	public void onQueryError(QueryException exception) {
-		addToList(new QueryLogValue(exception));
-	}
-	
-	private void addToList(QueryLogValue objectToAdd) {
-		synchronized (queries) {
-			queries.add(0, objectToAdd);
-			if(queries.size() > maxStored) {
-				queries.remove(queries.size() -1);
-			}
-		}
-	}
-	
-	public LinkedList<QueryLogValue> getLastQueries() {
-		return new LinkedList<QueryLogStatistic.QueryLogValue>(queries);
-	}
-	
-	public class QueryLogValue {
-		
-		private boolean error;
-		
-		private String queryString;
-		
-		private String facetQueryString;
-		
-		private String filterQueryString;
-		
-		private Integer qTime;
-		
-		private Long results;
-		
-		public QueryLogValue(QueryException exception) {
-			error = true;
-			queryString = exception.getQuery().getQuery();
-			facetQueryString = createString(exception.getQuery().getFacetQuery());
-			filterQueryString = createString(exception.getQuery().getFilterQueries());
-			qTime = -1;
-			results = new Long(0);
-		}
-		
-		@SuppressWarnings("unchecked")
-		public QueryLogValue(QueryResponse response) {
-			error = false;
-			queryString = (String) ((NamedList<Object>)response.getResponseHeader().get("params")).get("q");
-			facetQueryString = createFacetQuery(response.getFacetFields());
-			filterQueryString = (String) ((NamedList<Object>)response.getResponseHeader().get("params")).get("fq");
-			qTime = response.getQTime();
-			results = response.getResults().getNumFound();
-		}
-		
-		private String createFacetQuery(List<FacetField> facetFields) {
-			if(facetFields == null || facetFields.isEmpty()) {
-				return "";
-			}
-			StringBuffer buff = new StringBuffer();
-			for(FacetField query:facetFields) {
-				buff.append(query.getName());
-				buff.append("(" + query.getValueCount() + ") ");
-			}
-			return buff.toString();
-		}
-
-		private String createString(String[] facetQuery) {
-			if(facetQuery == null || facetQuery.length == 0) {
-				return "";
-			}
-			StringBuffer buff = new StringBuffer();
-			for(String query:facetQuery) {
-				buff.append(query);
-			}
-			return buff.toString();
-		}
-		
-		@Override
-		public String toString() {
-			return super.toString();
-		}
-
-		public boolean isError() {
-			return error;
-		}
-
-		public String getQueryString() {
-			return queryString;
-		}
-
-		public String getFacetQueryString() {
-			return facetQueryString;
-		}
-
-		public Integer getQTime() {
-			return qTime;
-		}
-
-		public String getFilterQueryString() {
-			return filterQueryString;
-		}
-
-		public Long getResults() {
-			return results;
-		}
-		
-		/**
-		 * Gets a comma separated values representation of this log entry
-		 * 
-		 * @return
-		 */
-		
-		public String getCSV(){
-			return String.valueOf(error) + "," +
-							queryString + "," +
-							facetQueryString + "," +
-							filterQueryString + "," +
-							qTime.toString() + "," +
-							results.toString();	
-		}
-	}
-
+  
+  private static int maxStored;
+  
+  private List<QueryLogValue> queries;
+  
+  @Inject
+  public QueryLogStatistic() {
+    super();
+    maxStored = Integer.parseInt(SolrMeterConfiguration.getProperty("solr.queryLogStatistic.maxStored", "400"));
+    queries = Collections.synchronizedList(new LinkedList<QueryLogValue>());
+  }
+  
+  @Override
+  public void onExecutedQuery(QueryResponse response, long clientTime) {
+    addToList(new QueryLogValue(response));
+  }
+  
+  @Override
+  public void onFinishedTest() {}
+  
+  @Override
+  public void onQueryError(QueryException exception) {
+    addToList(new QueryLogValue(exception));
+  }
+  
+  private void addToList(QueryLogValue objectToAdd) {
+    synchronized (queries) {
+      queries.add(0, objectToAdd);
+      if(queries.size() > maxStored) {
+        queries.remove(queries.size() -1);
+      }
+    }
+  }
+  
+  public LinkedList<QueryLogValue> getLastQueries() {
+    return new LinkedList<QueryLogStatistic.QueryLogValue>(queries);
+  }
+  
+  public class QueryLogValue {
+    
+    private boolean error;
+    
+    private String queryString;
+    
+    private String facetQueryString;
+    
+    private String filterQueryString;
+    
+    private Integer qTime;
+    
+    private Long results;
+    
+    public QueryLogValue(QueryException exception) {
+      error = true;
+      queryString = exception.getQuery().getQuery();
+      facetQueryString = createString(exception.getQuery().getFacetQuery());
+      filterQueryString = createString(exception.getQuery().getFilterQueries());
+      qTime = -1;
+      results = new Long(0);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public QueryLogValue(QueryResponse response) {
+      Object parameter;
+      error = false;
+      parameter = (String) ((NamedList<Object>)response.getResponseHeader().get("params")).get("q");
+      if (parameter != null) {
+        queryString = parameter.toString();
+      }
+      facetQueryString = createFacetQuery(response.getFacetFields());
+      parameter = ((NamedList<Object>)response.getResponseHeader().get("params")).get("fq");
+      if (parameter != null) {
+        filterQueryString = parameter.toString();
+      }
+      qTime = response.getQTime();
+      results = response.getResults().getNumFound();
+    }
+    
+    private String createFacetQuery(List<FacetField> facetFields) {
+      if(facetFields == null || facetFields.isEmpty()) {
+        return "";
+      }
+      StringBuffer buff = new StringBuffer();
+      for(FacetField query:facetFields) {
+        buff.append(query.getName());
+        buff.append("(" + query.getValueCount() + ") ");
+      }
+      return buff.toString();
+    }
+    
+    private String createString(String[] facetQuery) {
+      if(facetQuery == null || facetQuery.length == 0) {
+        return "";
+      }
+      StringBuffer buff = new StringBuffer();
+      for(String query:facetQuery) {
+        buff.append(query);
+      }
+      return buff.toString();
+    }
+    
+    @Override
+    public String toString() {
+      return super.toString();
+    }
+    
+    public boolean isError() {
+      return error;
+    }
+    
+    public String getQueryString() {
+      return queryString;
+    }
+    
+    public String getFacetQueryString() {
+      return facetQueryString;
+    }
+    
+    public Integer getQTime() {
+      return qTime;
+    }
+    
+    public String getFilterQueryString() {
+      return filterQueryString;
+    }
+    
+    public Long getResults() {
+      return results;
+    }
+    
+    /**
+     * Gets a comma separated values representation of this log entry
+     * 
+     * @return
+     */
+    
+    public String getCSV(){
+      return String.valueOf(error) + "," +
+      queryString + "," +
+      facetQueryString + "," +
+      filterQueryString + "," +
+      qTime.toString() + "," +
+      results.toString();	
+    }
+  }
+  
 }
